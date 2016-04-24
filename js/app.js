36a7e6c9d25696b77a5c4c1e0f6414d759c57d4b
Libbygi <isenstein.libby@gmail.com>
2016-04-24 15:58:33 -0400

    successfully pulls reddit API and appends to main

--- js/app.js
+++ js/app.js
@@ -9,31 +9,74 @@ var $mashable = $('#mashable');
 var $reddit = $('#reddit');
 var $digg = $('#digg');
 
+var $loader = $('#popUp.loader');
+var $main = $('#main');
+
 window.onload = function(){
+  $loader.removeClass('hidden');
+  getReddit();
 
 };
 
-function testDropdown() {
+
+function getReddit() {
     $.ajax({
         url: "https://www.reddit.com/top.json",
         method: "GET",
         dataType: "json",
         success: function(response) {
-          var allData = response.data.children;
+          $loader.addClass('hidden');
 
+          var allData = response.data.children;
+          var newArray = [];
+
+          //Go through data from Reddit, pull out what we need, and create new objects that we append to newArray, in uniform format
           allData.forEach(function(item){
-            console.log(item.data.subreddit);
-            console.log(item.data.permalink);
-            console.log(item.data.title);
-            console.log(item.data.ups);
-            if (item.data.media){
-              console.log(item.data.media.oembed.thumbnail_url);
-            }
+              var newObj = {};
+              var title = item.data.title;
+              var category = item.data.subreddit;
+              var impressions = item.data.ups;
+              var url = 'http://www.reddit.com' + item.data.permalink;
+              if (item.data.media){
+                var image = item.data.media.oembed.thumbnail_url;
+              } else {
+                var image = "images/default-image.png";
+              };
+
+              newObj.title = title;
+              newObj.category = category;
+              newObj.impressions = impressions;
+              newObj.url = url;
+              newObj.image = image;
+
+              newArray.push(newObj);
+          });
+
+              var source = $('#article-template').html();
+              var template = Handlebars.compile(source);
+
+          //go through new array that we have created with streamlined data and append each to #main
+          newArray.forEach(function(item){
+              $main.append(template(item))
           })
+
+
+          console.log(newArray);
         }
     })
 }
-testDropdown();
+
+//NEXT TO DO
+// add pop up to Main
+// make dropdown menu work
+// hook up the other two APIs 
+
+//
+// function pullInfo(data){
+//   var source = $('#article-template').html();
+//   var template = Handlebars.compile(source);
+//   $("#main").append(template(data));
+// };
 
 $menu.children().on('click' , function(){
   console.log('childclicked');
@@ -41,13 +84,13 @@ $menu.children().on('click' , function(){
   $menuSpan.html('name of the thing');
 })
 
-var article = {
-  title: "jesse shawl won the lottery",
-  subhed: "But you'll never believe what happened next.",
-  url: "https://jesse.sh/awl"
-};
-
-var source = $('#article-template').html();
-var template = Handlebars.compile(source);
-//console.log(template(article));
-$("#main").append(template(article));
+// var article = {
+//   title: "jesse shawl won the lottery",
+//   category: "sports",
+//   url: "https://jesse.sh/awl"
+// };
+//
+// var source = $('#article-template').html();
+// var template = Handlebars.compile(source);
+// //console.log(template(article));
+// $("#main").append(template(article));
