5d82f0c6258e0ebb4ce0d5f07cfe62f45bf206e4
Libbygi <isenstein.libby@gmail.com>
2016-04-26 10:57:54 -0400

    adding other APIs

--- js/app.js
+++ js/app.js
@@ -5,11 +5,13 @@ var $loader = $('#popUp.loader');
 var $main = $('#main');
 var $article = $('.article');
 var $popUp = $('#popUp');
-var newArray = [];
 var $closePopUp = $('.closePopUp');
 var $popUpTitle = $('.popUpTitle');
 var $popUpSummary = $('.popUpSummary');
 var $popUpAction = $('.popUpAction');
+var source = $('#article-template').html();
+var template = Handlebars.compile(source);
+var newArray = [];
 var urlObj = {
   reddit: 'https://www.reddit.com/top.json',
   mashable: 'http://feedr-api.wdidc.org/mashable.json',
@@ -21,16 +23,125 @@ window.onload = function(){
   callApi(urlObj.reddit);
 };
 
+//Dropdown menu
+$menu.children().on('click' , function(){
+  //Changes News Source: XX to name of selected menu item
+  var sourceName = $(this).text().toLowerCase();
+  $menuSpan.html(sourceName);
+
+  //Shows or hides articles based on what is selected in dropdown
+  $body.find('.article').addClass('hidden');
+  $body.find('.' + sourceName).removeClass('hidden');
+  //$loader.removeClass('hidden');
+  callApi(urlObj[sourceName]);
+})
+
+function callApi(endpoint) {
+    $.ajax({
+        url: endpoint,
+        method: "GET",
+        dataType: "json",
+        success: function (response) {
+          if (endpoint === urlObj.reddit){
+            onRedditSuccess(response);
+          } else if (endpoint === urlObj.mashable){
+            onMashableSuccess(response);
+          } else if (endpoint === urlObj.digg){
+            onDiggSuccess(response);
+          }
+        }
+    })
+};
+
+function onRedditSuccess(response){
+  $loader.addClass('hidden');
+  var allData = response.data.children;
+  newArray = [];
+
+  //Go through data from Reddit, pull out what we need, and create new objects that we append to newArray, in uniform format
+  allData.forEach(function(item){
+      if (item.data.media){
+        var image = item.data.media.oembed.thumbnail_url;
+      } else {
+        var image = "images/default-image.png";
+      };
+
+      var newObj = {
+        title: item.data.title,
+        category: item.data.subreddit,
+        impressions: item.data.ups,
+        url: 'http://www.reddit.com' + item.data.permalink,
+        image: image,
+        id: item.data.id,
+        blog: 'Reddit'
+      }
+
+      newArray.push(newObj);
+  });
+
+  appendArticle(newArray);
+}
+
+function onMashableSuccess(response){
+  $loader.addClass('hidden');
+  var allData = response.new;
+  newArray = [];
+
+  allData.forEach(function(item){
+      if (item.feature_image){
+        var image = item.feature_image;
+      } else {
+        var image = "images/default-image.png";
+      };
+      var newObj = {
+        title: item.title,
+        category: item.channel,
+        impressions: item.shares.total,
+        url: item.link,
+        image: image,
+        content: item.content.plain,
+        id: item._id,
+        blog: 'Mashable'
+      }
+      newArray.push(newObj);
+  });
+  appendArticle(newArray);
+}
+
+function onDiggSuccess(response){
+  $loader.addClass('hidden');
+  var allData = response.data.feed;
+  newArray = [];
+
+  allData.forEach(function(item){
+      var newObj = {
+        title: item.content.title,
+        category: item.content.tags.name,
+        impressions: item.diggs.count,
+        url: item.content.url,
+        image: item.content.media.images[0].original_url,
+        content: item.content.description,
+        id: item.content.content_id,
+        blog: 'Digg'
+      }
+      newArray.push(newObj);
+  });
+  appendArticle(newArray);
+}
+
+//go through new array that we have created with streamlined data and append each to #main
+function appendArticle(newArray){
+  newArray.forEach(function(item){
+      $main.append(template(item));
+  })
+}
+
 //listens for click on article to show popup
-$main.on('click', '.article', function () {
+$main.on('click', '.article', function(){
    $popUp.removeClass('hidden loader');
    var clickedId = $(this).attr('id');
    matchId(newArray, clickedId);
 });
-
-$closePopUp.on('click', function(){
-  $popUp.addClass('hidden');
-})
 
 // function to check for matched ID and then pull appropriate info for popup
 function matchId(data, id){
@@ -47,70 +158,10 @@ function matchId(data, id){
   })
 };
 
-function callApi(endpoint) {
-    $.ajax({
-        url: endpoint,
-        method: "GET",
-        dataType: "json",
-        success: function (response) {
-          onRedditSuccess(response);
-          //onMashableSuccess(response);
-        }
-    })
-};
-
-function onRedditSuccess(response){
-  $loader.addClass('hidden');
-  var allData = response.data.children;
-
-  //Go through data from Reddit, pull out what we need, and create new objects that we append to newArray, in uniform format
-  allData.forEach(function(item){
-      var newObj = {};
-      var title = item.data.title;
-      var category = item.data.subreddit;
-      var impressions = item.data.ups;
-      var url = 'http://www.reddit.com' + item.data.permalink;
-      var author = item.data.author;
-      var id = item.data.id;
-      if (item.data.media){
-        var image = item.data.media.oembed.thumbnail_url;
-      } else {
-        var image = "images/default-image.png";
-      };
-
-      newObj.title = title;
-      newObj.category = category;
-      newObj.impressions = impressions;
-      newObj.url = url;
-      newObj.image = image;
-      newObj.author = author;
-      newObj.id = id;
-      newObj.blog = 'Reddit';
-
-      newArray.push(newObj);
-  });
-
-  var source = $('#article-template').html();
-  var template = Handlebars.compile(source);
-
-      //go through new array that we have created with streamlined data and append each to #main
-      newArray.forEach(function(item){
-        $main.append(template(item))
-      })
-}
-
-//Dropdown menu
-$menu.children().on('click' , function(){
-  //Changes News Source: XX to name of selected menu item
-  var sourceName = $(this).text().toLowerCase();
-  $menuSpan.html(sourceName);
-
-  //Shows or hides articles based on what is selected in dropdown
-  $body.find('.article').addClass('hidden');
-  $body.find('.' + sourceName).removeClass('hidden');
-  callApi(urlObj[sourceName]);
-
+$closePopUp.on('click', function(){
+  $popUp.addClass('hidden');
 })
+
 
 // var article = {
 //   title: "jesse shawl won the lottery",
